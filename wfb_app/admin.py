6b1a9e77b687e4767acaeb26b0167569768d699e
Marcin <sommer.marcin@gmail.com>
2020-12-16 21:35:33 +0100

    admin panel edycja

--- wfb_app/admin.py
+++ wfb_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from wfb_app.models import GameResults, Armys
+
+
+class GameResultsAdmin(admin.ModelAdmin):
+    list_display = ("user", "battle_points", "objective", "objective_type", "game_rank", "opponent", "date")
+    list_editable = ("battle_points", "objective", "objective_type", "game_rank", "opponent")
+
+class ArmysAdmin(admin.ModelAdmin):
+    list_display = ("name", "short_name", "description")
+    list_editable = ("short_name", "description")
+
+
+admin.site.register(GameResults, GameResultsAdmin)
+admin.site.register(Armys, ArmysAdmin)
